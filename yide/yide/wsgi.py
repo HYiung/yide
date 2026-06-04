9c6232e48951290e651a19800359e12c54ec0fe4
HYoung <12284974+hyoung98@user.noreply.gitee.com>
2026-06-04 17:53:12 +0800

    try for vercel

--- yide/yide/wsgi.py
+++ yide/yide/wsgi.py
@@ -7,20 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+# yide/yide/wsgi.py
+
 import os
 import sys
-
-# 获取当前文件所在目录
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# 获取项目根目录（yide/yide 的上一层）
-project_root = os.path.dirname(current_dir)
-
-# 将项目根目录添加到 Python 路径
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yide.yide.settings')
+# 👈 终极绝招：强行把外层的 yide 文件夹加入到 Python 的查找路径中
+# 这样不管是找 yide.settings 还是 yide.yide.settings，Python 都能找到了
+current_dir = os.path.dirname(os.path.abspath(__file__)) # 这一层是第二个yide
+parent_dir = os.path.dirname(current_dir)               # 这一层是第一个yide
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+    sys.path.insert(0, current_dir) # 保险起见，两层都塞进去
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yide.settings')
 
 application = get_wsgi_application()
