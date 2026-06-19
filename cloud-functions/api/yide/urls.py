9ffc1f76216cc84cc78a89644d1f4b9462039eb0
root <root@HYoung.localdomain>
2026-06-19 17:50:21 +0800

    debug: add /__diag_static__ endpoint to debug static file serving on EdgeOne

--- cloud-functions/api/yide/urls.py
+++ cloud-functions/api/yide/urls.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.contrib.staticfiles.views import serve as static_serve
+from django.http import HttpResponse
+from django.contrib.staticfiles import finders
+
+def diag_static(request):
+    """诊断：检查 staticfiles 是否能找到文件"""
+    from django.conf import settings
+    lines = [
+        f"STATIC_URL: {settings.STATIC_URL}",
+        f"DEBUG: {settings.DEBUG}",
+    ]
+    for p in ['admin/css/base.css', 'admin/js/theme.js', 'admin/css/login.css']:
+        found = finders.find(p)
+        lines.append(f"finders.find('{p}'): {found}")
+    return HttpResponse('\n'.join(lines), content_type='text/plain')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__diag_static__', diag_static),
     path('', include('web.urls')),
 ]
 
